7304b0be49b18fd2e97ac70c8b5bb176aff4d118
NeKo <feliz.chauz@gmail.com>
2013-04-29 12:13:18 +0400

    Initial Commit

---
+++ childtalesall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  childtalesall
+//
+//  Created by neko on 29.04.13.
+//  Copyright (c) 2013 Lev Natalya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ childtalesall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  childtalesall
+//
+//  Created by neko on 29.04.13.
+//  Copyright (c) 2013 Lev Natalya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
